bcf66bbfe4c50fcd183ddca348907b9726d898c3
Taranveer Bains <taranveer786@gmailcom>
2023-10-18 13:06:23 -0700

    yolo

--- main.go
+++ main.go
@@ -14,10 +14,17 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	env := os.Getenv("FOO_ENV")
+	if "" == env {
+		env = "development"
 	}
+
+	godotenv.Load(".env." + env + ".local")
+	if "test" != env {
+		godotenv.Load(".env.local")
+	}
+	godotenv.Load(".env." + env)
+	godotenv.Load() // The Original .env
 
 	db, err := models.Open(models.PostgresConfig{
 		Host:     os.Getenv("PSQL_HOST"),
